9ac0be3c10f2a636739c3b1b229f0085ffa16350
Duohong <duohongopen@gmail.com>
2019-08-17 12:39:31 -0400

    rabbit added

--- FlaskAlchemy/app.py
+++ FlaskAlchemy/app.py
@@ -10,10 +10,14 @@ import datetime
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+@app.route("/")
+def index():
+    return "duohong";
+
 # database
 # easy to switch to ms sql server here.
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "db.sqlite")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
@@ -30,50 +34,50 @@ class User(db.Model):
 # user schema
 class UserSchema(ma.Schema):
     class Meta:
-        fields = ('id', 'username')
+        fields = ("id", "username")
 
 # init schema
 user_schema = UserSchema(strict=True)
 users_schema = UserSchema(many=True, strict=True)
 
 # signup
-@app.route('/signup')
+@app.route("/signup")
 def signup():
-    username = request.josn['username']
-    password = request.josn['password']
-    hashed_password = generate_password_hash(password, method='sha256')
+    username = request.josn["username"]
+    password = request.josn["password"]
+    hashed_password = generate_password_hash(password, method="sha256")
     new_user = User(username, hashed_password)
     try: 
         db.session.add(new_user)
         db.session.commit()
     except:
-        return jsonify({'message' : 'Database error!'}), 500
+        return jsonify({"message" : "Database error!"}), 500
 
-    token = jwt.encode({'id' : new_user.id, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=20)}, 'MY_SECRET_KEY')
-    return jsonify({'token' : token.decode('UTF-8')})
+    token = jwt.encode({"id" : new_user.id, "exp" : datetime.datetime.utcnow() + datetime.timedelta(minutes=20)}, "MY_SECRET_KEY")
+    return jsonify({"token" : token.decode("UTF-8")})
 
 # signin
-@app.route('/signin')
+@app.route("/signin")
 def signin():
     auth = request.authorization
     if not auth or not auth.username or not auth.password:
-        return make_response('Could not verify', 401, {'WWW-Authenticate' : 'Basic realm="Login required!"'})
+        return make_response("Could not verify", 401, {"WWW-Authenticate" : 'Basic realm="Login required!"'})
 
     user = User.query.filter_by(username=auth.username).first()
     if not user:
-        return make_response('Could not verify', 401, {'WWW-Authenticate' : 'Basic realm="Login required!"'})
+        return make_response("Could not verify", 401, {"WWW-Authenticate" : 'Basic realm="Login required!"'})
 
     if check_password_hash(user.hashed_password, auth.password):
-        token = jwt.encode({'id' : user.id, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=20)}, 'MY_SECRET_KEY')
-        return jsonify({'token' : token.decode('UTF-8')})
+        token = jwt.encode({"id" : user.id, "exp" : datetime.datetime.utcnow() + datetime.timedelta(minutes=20)}, "MY_SECRET_KEY")
+        return jsonify({"token" : token.decode("UTF-8")})
 
-    return make_response('Could not verify', 401, {'WWW-Authenticate' : 'Basic realm="Login required!"'})
+    return make_response("Could not verify", 401, {"WWW-Authenticate" : 'Basic realm="Login required!"'})
 
 # test
-@app.route('/test')
+@app.route("/test")
 def test():
-    return jsonify({'Hello World!' : datetime.datetime.utcnow()})
+    return jsonify({"Hello World!" : datetime.datetime.utcnow()})
 
 # run server
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
